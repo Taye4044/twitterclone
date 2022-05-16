4cc946f04db9564d8809912077177ba5d5667ca9
Yohannes Taye <ydemisew@gmail.com>
2022-05-15 23:56:32 -0400

    twitter clone

---
+++ posts/forms.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
---
+++ posts/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+
+
+def index(request):
+    # If the method is POST
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        # If the form is valid
+        if form.is_valid():
+            # Yes, Save
+            form.save()
+
+            # Redirect to Home
+            return HttpResponseRedirect('/')
+
+        else:
+            # No, Show Error
+            return HttpResponseRedirect(form.errors.as_json())
+
+    # Get all posts, limit = 20
+    posts = Post.objects.all().order_by('-created_at')[:20]
+
+    # Show
+    return render(request, 'posts.html',
+                  {'posts': posts})
+
+
+def delete(request, post_id):
+    # Find post
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
+
+
+def edit(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = PostForm
+        return render(request, "edit.html", {"post": post, 'form': form})
+
+
+def likebtn(request, post_id):
+    like = Post.objects.get(id=post_id)
+    like.likes += 1
+    like.save()
+    return HttpResponseRedirect('/')
